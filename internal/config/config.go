27e329ed48117299f8af6afcb9e2ae97fe02590a
Hickar <hickar@icloud.com>
2021-07-27 21:04:34 +0300

    Configuration file unmarshalling into Config type

---
+++ internal/config/config.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+type Config struct {
+	App      AppConfig      `json:"app"`
+	Server   ServerConfig   `json:"server"`
+	Database DatabaseConfig `json:"database"`
+}
+
+type AppConfig struct {
+	DateFormat   string `json:"date_format"`
+	LogDirectory string `json:"log_directory"`
+	LogFilePath  string `json:"log_file_path"`
+	LogFormat    string `json:"log_format"`
+}
+
+type ServerConfig struct {
+	Mode string `json:"mode"`
+	Port int    `json:"port"`
+}
+
+type DatabaseConfig struct {
+	Type     string `json:"type"`
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Name     string `json:"name"`
+}
+
+func New(filePath string) *Config {
+	jsonFile, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("Can't open config.json: %s", err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		log.Fatalf("Can't read config.json: %s", err)
+	}
+
+	var config Config
+	if err := json.Unmarshal(byteValue, &config); err != nil {
+		log.Fatalf("Error during config unmarshaling: %s", err)
+	}
+
+	return &config
+}
