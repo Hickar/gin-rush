50fddf1d8adce992a7d04c554aef2b20f3c1abe4
Hickar <hickar@icloud.com>
2021-07-27 20:38:49 +0300

    GIN server start

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.New()
+
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	api := router.Group("/api")
+	{
+		api.GET("user/:id", nil)
+		api.POST("user", nil)
+		api.PATCH("user", nil)
+		api.DELETE("user/:id", nil)
+
+		api.POST("/authorize/email/challenge/:code", nil)
+		api.POST("/authorize", nil)
+	}
+
+	if err := router.Run(":8000"); err != nil {
+		log.Fatalf("Cannot start GIN server: %s", err)
+	}
 }
